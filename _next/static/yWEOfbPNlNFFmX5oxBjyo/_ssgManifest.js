9f165cc6b8bc9a82204a666d59e18dfd173b054d
juan-solops <casajuanlau@gmail.com>
2026-05-12 16:22:30 +0200

    Updates

---
+++ _next/static/yWEOfbPNlNFFmX5oxBjyo/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fblog","\u002F[locale]\u002Fblog\u002F[slug]","\u002F[locale]\u002Fblog\u002Fcategory\u002F[category]","\u002F[locale]\u002Fcontact"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
